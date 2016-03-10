8e1392e89d1471116340c657c7ce4c1ace063abe
yuyc <yuyc@boqii.com>
2016-03-10 10:35:07 +0800

    delete test

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#notes
-=======
-# docs
->>>>>>> 02c9f03b80cc95a05b125413ae27001e939abdb7
+#yuyc docs
--- test/bq_login.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-
-import sys
-import getopt
-opt,args = getopt.getopt(sys.argv[1:],"-l",['hehe'])
-print opt
-print '==='
-print args
